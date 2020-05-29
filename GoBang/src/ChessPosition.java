8a7a810633c74484d623b6c25922c47fcaa13685
ASUS <ASUS@192.168.40.1>
2020-05-29 19:50:00 +0800

    林皆名

---
+++ GoBang/src/ChessPosition.java
@@ -0,0 +1,11 @@
+public class ChessPosition {
+	public int Listi,Listj;
+	
+	public ChessPosition() {
+		
+	}
+	public ChessPosition(int Listi,int Listj) {
+		this.Listi=Listi;
+		this.Listj=Listj;
+	}
+}
